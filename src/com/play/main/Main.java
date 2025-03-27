00b9223ebe0c16148f46ec66dcbd508ae901df26
Vinayak-Gawade <vinayakgawade35@gmail.com>
2025-03-28 00:21:41 +0530

    Creating simple calculator using java GUI

--- src/com/play/main/Main.java
+++ src/com/play/main/Main.java
@@ -1,9 +1,12 @@
-package play;
+package com.play.main;
+
+import com.play.calculator.Calculator;
 
 public class Main {
 
 	public static void main(String[] args) {
         System.out.println("Hello from java-playground!");
+        new Calculator();
 	}
 
 }
